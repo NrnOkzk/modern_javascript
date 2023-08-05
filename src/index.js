38e77d19ea414ae5079d45a1481ba67cb7e1b4d6
NrnOkzk <104541302+NrnOkzk@users.noreply.github.com>
2023-08-05 14:00:35 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+/**
+ * const, letなどの変数宣言
+ */
+
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可能
+// let val2 = "let変数を再宣言"; //そもそもJSとしてエラー
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // constは上書き不可能
+// // val3 = "const変数を上書き";
+
+// // constは再宣言不可能
+// const val3="const変数を再宣言"
+
+// constで定義したobjectはプロパティの変更が可能
+// const val4 = {
+//   name: "narin",
+//   age: 25
+// };
+// val4.age = 26;
+// val4.address = "kanagawa";
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
