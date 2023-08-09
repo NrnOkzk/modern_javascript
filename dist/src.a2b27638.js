f270386e022509ac8b3e38787ddb422816db0948
narin <104541302+NrnOkzk@users.noreply.github.com>
2023-08-09 00:14:16 +0000

    スプレッド構文 ...

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -232,11 +232,39 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 /**
  * デフォルト値、引き数など
  */
-var sayHello = function sayHello() {
-  var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "ゲスト";
-  return console.log("\u3053\u3093\u306B\u3061\u306F!".concat(name, "\u3055\u3093!"));
-};
-sayHello();
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
+// sayHello();
+
+/**
+ * スプレッド構文 ...
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+var arr4 = [10, 20];
+var arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+var arr8 = arr4; // これは参照渡しになるので、どちらか変更するともう片方も影響を受ける
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
